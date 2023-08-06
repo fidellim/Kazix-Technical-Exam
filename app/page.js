2aea7c13c83dedfba8ad0ed45838aff45ab3bfa1
fidellim <lim.fidel@gmail.com>
2023-08-06 14:30:22 +0400

    refactor: import hero and promo item components

--- app/page.js
+++ app/page.js
@@ -2,37 +2,37 @@
 
 import Link from 'next/link'
 import { useLanguage } from './LanguageContext'
-import { instrument_sans } from './fonts'
+import Hero from './components/Hero'
+import PromoItem from './components/PromoItem'
 
 export default function Home() {
     const { getText } = useLanguage()
 
     return (
         <main className="pt-[30px] px-4 bg-white">
-            <div className="HeaderImages max-w-[1024px] mx-[auto] relative rounded-3xl bg-welcome_banner bg-no-repeat bg-cover">
-                <div className="flex-col justify-start py-[40px] pl-[55px] items-start gap-2 inline-flex">
-                    <div className="relative mr-4">
-                        <span className="text-yellow-400 text-[32px] sm:text-[68px] font-normal leading-[32px] sm:leading-[69px]">
-                            €200
-                            <br />
-                        </span>
-                        <span className="text-white uppercase text-[32px] sm:text-[68px] font-normal leading-[32px] sm:leading-[69px]">
-                            {getText('welcome_bonus')}
-                        </span>
-                    </div>
-                    <div
-                        className={`text-white text-base font-normal leading-[34px] ${instrument_sans.className} mr-4`}
-                    >
-                        {getText('receive_bonus')}
-                    </div>
-                    <div className="ButtonsV2 p-3 bg-yellow-400 rounded-xl justify-center items-center inline-flex mr-4">
-                        <Link href="/register">
-                            <div className="uppercase text-teal-900 text-base font-normal leading-7">
-                                {getText('register')}
-                            </div>
-                        </Link>
-                    </div>
-                </div>
+            <Hero
+                welcomeBonus={getText('welcome_bonus')}
+                receiveBonus={getText('receive_bonus')}
+                register={getText('register')}
+                bg_banner="bg-welcome_banner"
+                btn_link="/register"
+            />
+
+            <div className="mx-[auto] mt-[18px] mb-[26px] max-w-[1024px] justify-start items-start gap-[18px] flex flex-col md:flex-row">
+                <PromoItem
+                    headingOne={getText('bet_sports.heading_one')}
+                    headingTwo={getText('bet_sports.heading_two')}
+                    buttonText={getText('bet_sports.button_text')}
+                    bg_banner="bg-bet_sports_banner"
+                    btn_link="/matches"
+                />
+                <PromoItem
+                    headingOne={getText('play_casino.heading_one')}
+                    headingTwo={getText('play_casino.heading_two')}
+                    buttonText={getText('play_casino.button_text')}
+                    bg_banner="bg-play_casino_banner"
+                    btn_link="/games"
+                />
             </div>
         </main>
     )
