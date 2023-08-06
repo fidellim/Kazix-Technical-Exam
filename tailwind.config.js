331c48dafaed5791de833225f0bb47452bbdd4ec
fidellim <lim.fidel@gmail.com>
2023-08-06 14:24:42 +0400

    style: add bg images for promo items

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,8 @@ module.exports = {
                 'gradient-conic':
                     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
                 welcome_banner: "url('/assets/welcome_banner.png')",
+                bet_sports_banner: "url('/assets/bet_sports_bg.png')",
+                play_casino_banner: "url('/assets/play_casino_bg.png')",
             },
         },
     },
