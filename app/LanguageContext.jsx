2ce9072ed8ada984adb1cac1a7cc46873429e8aa
fidellim <lim.fidel@gmail.com>
2023-08-06 14:25:22 +0400

    feat: getText func can retrieve nested objects

--- app/LanguageContext.jsx
+++ app/LanguageContext.jsx
@@ -17,7 +17,22 @@ export const LanguageProvider = ({ children }) => {
     const [currentLang, setCurrentLang] = useState('en')
 
     const getText = (key) => {
-        return languages[currentLang][key] || languages['en'][key] || key
+        const languageData = languages[currentLang]
+
+        const nestedKeyPath = key.split('.')
+        let translation = languageData
+
+        for (const nestedKey of nestedKeyPath) {
+            if (translation[nestedKey]) {
+                translation = translation[nestedKey]
+            } else {
+                // If the nested key is not found in the current language, fallback to English.
+                translation = languages['en']
+                break
+            }
+        }
+
+        return translation
     }
 
     return (
